f119a579208948c2842bcebc1d2c56cc2b7abf97
Masuma01~ <javariamasooma.bscssef20@iba-suk.edu.pk>
2023-11-07 11:05:09 +0500

    Completed

--- myportfolio/src/Pages/about/About.jsx
+++ myportfolio/src/Pages/about/About.jsx
@@ -1,35 +1,33 @@
-import { useEffect, useState } from 'react';
-//import { homeData } from '../../data/data';
-import HeroImg from './heroImg 1.svg';
-import { getAllNotes } from '../../api/http';
-function About(){
-    const [aboutData, setAboutData]=useState('');
-    useEffect(()=>{
-        getAllNotes().then(response=>{
-            setAboutData(response);
-        });
-    },[]);
-          
-    return (
-        <div className="flex flex-row justify-between pl-8">
-            <div className="flex flex-col font-bold gap-4 text-blue-900 bg-opacity-100"> 
-                <div className='text-3xl font-Poppins '>
-                    <h1>Hi!</h1>
-                    <h2 className=''>I am {aboutData.last_name}. <br/> 
-                        A Front-End Developer. </h2></div>
-                    <div className='text-sm border '>
-                        <p className=' m-3'>
-                        {aboutData.description}
-                           </p>
-                    </div>
-                </div>
-    
-                <div className='object-fill w-56 flex-shrink-0 mr-10'>
-                  <img src={HeroImg} alt="aaa" />
-                </div>
-    
+import { useEffect, useState } from "react";
+import HeroImg from "./heroImg 1.svg";
+import { getAllNotes } from "../../api/http";
+function About() {
+  const [aboutData, setAboutData] = useState("");
+  useEffect(() => {
+    getAllNotes().then((response) => {
+      setAboutData(response);
+    });
+  }, []);
+
+  return (
+    <div className="flex flex-row justify-between pl-8">
+      <div className="flex flex-col font-bold gap-4 text-blue-900 bg-opacity-100">
+        <div className="text-3xl font-Poppins ">
+          <h1>Hi!</h1>
+          <h2 className="">
+            I am {aboutData.last_name}. <br />A Front-End Developer.{" "}
+          </h2>
         </div>
-      )
+        <div className="text-sm border ">
+          <p className=" m-3">{aboutData.description}</p>
+        </div>
+      </div>
+
+      <div className="object-fill w-56 flex-shrink-0 mr-10">
+        <img src={HeroImg} alt="aaa" />
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- myportfolio/src/components/header/Header.jsx
+++ myportfolio/src/components/header/Header.jsx
@@ -3,63 +3,70 @@ import { Link } from "react-router-dom";
 import { getAllNotes } from "../../api/http";
 //import Contact from "../../Pages/contact/Contact";
 const menulist = [
-    {
-      id: 1,
-      title: "Home",
-      path:'/',
-    },
-    {
-      id: 2,
-      title: "About",
-      path:'/About',
-    },
-    {
-      id: 3,
-      title: "Passions",
-      path:'/Passion',
-    },
-    {
-      id: 4,
-      title: "Portfolio",
-      path:'/Port',
-    }
-    
-  ];
-export function Header(){
-  const [headerData, setHeaderData]=useState('');
-  useEffect(()=>{
-      getAllNotes().then(response=>{
-          setHeaderData(response);
-      });
-  },[]);
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "About",
+    path: "/About",
+  },
+  {
+    id: 3,
+    title: "Passions",
+    path: "/Passion",
+  },
+  {
+    id: 4,
+    title: "Portfolio",
+    path: "/Port",
+  },
+];
+export function Header() {
+  const [headerData, setHeaderData] = useState("");
+  useEffect(() => {
+    getAllNotes().then((response) => {
+      setHeaderData(response);
+    });
+  }, []);
 
-        const [theme, setTheme]= useState('green');
-        console.log('check the render' , theme);
-        useEffect(()=>{
-            console.log("Init", theme);
-        
-          },[theme]);
-      return (
-        <div className="flex flex-row justify-between content-center p-8">
-          <div>
-            <h2 className="text-blue-900 text-center font-poppins text-25 font-bold tracking-wider" onClick={()=>setTheme('red')}>
-              {headerData.first_name} <span className="text-blue-700">{headerData.last_name}</span>
-            </h2>
-          </div>
-     
-          <div className="flex gap-4 font-poppins">
-            {menulist && menulist.map((item) => (
-              <div key={item.id}>
-                <Link to={item.path} className=" hover:border-[1px] border-green-800 rounded-full px-3 py-1 cursor-pointer">
-                  {item.title}
-                </Link>
-            
-              </div>
-            ))}
-            <div className="text-white text-center font-poppins w-25 tracking-wide p-1 rounded-full bg-blue-500 hover:bg-blue-700">
-                <Link to='/Contact'><p>Contact Me</p></Link>
+  const [theme, setTheme] = useState("green");
+  console.log("check the render", theme);
+  useEffect(() => {
+    console.log("Init", theme);
+  }, [theme]);
+  return (
+    <div className="flex flex-row justify-between content-center p-8">
+      <div>
+        <h2
+          className="text-blue-900 text-center font-poppins text-25 font-bold tracking-wider"
+          onClick={() => setTheme("red")}
+        >
+          {headerData.first_name}{" "}
+          <span className="text-blue-700">{headerData.last_name}</span>
+        </h2>
+      </div>
+
+      <div className="flex gap-4 font-poppins">
+        {menulist &&
+          menulist.map((item) => (
+            <div key={item.id}>
+              <Link
+                to={item.path}
+                className=" hover:border-[1px] border-green-800 rounded-full px-3 py-1 cursor-pointer"
+              >
+                {item.title}
+              </Link>
             </div>
-          </div>
+          ))}
+        <div className="text-white text-center font-poppins w-25 tracking-wide p-1 rounded-full bg-blue-500 hover:bg-blue-700">
+          <Link to="/Contact">
+            <p>Contact Me</p>
+          </Link>
         </div>
-      );
+      </div>
+    </div>
+  );
 }
